65e8c0bbcc99ba96540a1af3fb23d76d03349d10
Harshal Dhote <dhoteh020@gmail.com>
2023-10-18 22:03:45 +0530

    commit oct_18_3

--- NetBeans/Assignments/src/assignments/One/q9.java
+++ NetBeans/Assignments/src/assignments/One/q9.java
@@ -21,11 +21,11 @@ package assignments.One;
 
 public class q9 {
     public static void main(String[] args) {
-       int n = 7; // Height of the letters
+       int n = 7; 
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                // Display 'T'
+                
                 if (i == 0 || j == n / 2) {
                     System.out.print("T");
                 } else {
@@ -35,14 +35,14 @@ public class q9 {
            
             System.out.print("   "); 
 
+           
             for (int j = 0; j < n; j++) {
-                if (j == 0 || (i == n / 2 && j >= n / 2) || (i == j && j >= n / 2) || (i + j == n - 1 && j <= n / 2)) {
+                if (j == 0 || i == n - 4 || i + j == n - 2) {
                     System.out.print("K");
                 } else {
                     System.out.print(" ");
                 }
-            }
-            System.out.println();
+            }            System.out.println();
         }
     }
 }
