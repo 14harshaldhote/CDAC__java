91b9b1346dd859fc7a729203d178d7afc08e7034
Harshal Dhote <dhoteh020@gmail.com>
2023-10-18 21:46:50 +0530

    commit oct_18_2

--- NetBeans/Assignments/src/assignments/One/q9.java
+++ NetBeans/Assignments/src/assignments/One/q9.java
@@ -7,13 +7,36 @@ package assignments.One;
 /**
  *
  * @author harshalsMac
+ *  for (int j = 0; j < n; j++) {
+                // Display 'T'
+                if (i == 0 || j == n / 2) {
+                    System.out.print("T");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
  */
 
-class KPatternGenerator {
-    public static void generateKPattern(int n) {
+
+public class q9 {
+    public static void main(String[] args) {
+       int n = 7; // Height of the letters
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j == 0 || (i == j && j >= n / 2) || (i + j == n - 1 && j >= n / 2)) {
+                // Display 'T'
+                if (i == 0 || j == n / 2) {
+                    System.out.print("T");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+           
+            System.out.print("   "); 
+
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || (i == n / 2 && j >= n / 2) || (i == j && j >= n / 2) || (i + j == n - 1 && j <= n / 2)) {
                     System.out.print("K");
                 } else {
                     System.out.print(" ");
@@ -21,12 +44,5 @@ class KPatternGenerator {
             }
             System.out.println();
         }
-    }
-}
-
-public class q9 {
-    public static void main(String[] args) {
-        int n = 10; 
-        KPatternGenerator.generateKPattern(n);
     }
 }
