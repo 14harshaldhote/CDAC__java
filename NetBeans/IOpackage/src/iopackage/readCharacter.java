60ebe4a05f017c55ddf5f45169e8ebce8ec4594e
Harshal Dhote <dhoteh020@gmail.com>
2023-10-20 17:43:49 +0530

    commit oct20

--- NetBeans/IOpackage/src/iopackage/readCharacter.java
+++ NetBeans/IOpackage/src/iopackage/readCharacter.java
@@ -12,7 +12,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class readCharacter  {
-   
+    
+    void readCharacter() throws IOException{
+        char c;
+        String input;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Provide Sentence to read charcetr by character : ");
+         br.readLine();
+        while (!(input = br.readLine()).equals("q")) {
+            int length = input.length();
+            System.out.println("Characters in the sentence:");
+            
+            for (int i = 0; i < length; i++) {
+                c = input.charAt(i); // Store the character in 'c'
+                System.out.println(c);
+            }
+            
+            System.out.println("Provide another sentence or type 'q' to stop: ");
+        }
+        
+    }
+    
     
     public static void main(String[] args) {
        char c;
@@ -25,7 +45,7 @@ public class readCharacter  {
             System.out.println("Characters in the sentence:");
 
             for (int i = 0; i < length; i++) {
-                c = input.charAt(i); 
+                c = input.charAt(i); // Store the character in 'c'
                 System.out.println(c);
             }
 
