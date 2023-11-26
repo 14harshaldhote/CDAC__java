25e2959ba3a9d206cb00a991532f89f240051a09
Harshal Dhote <dhoteh020@gmail.com>
2023-11-26 18:37:36 +0530

    homeShopping

---
+++ AdvanceJava/homePractice/src/java/shopping/ecomShop.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package shopping;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="ecomShop",urlPatterns = "/ecomShop")
+public class ecomShop extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ecomShop</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            String proName=request.getParameter("pn");
+            String proQuan=request.getParameter("pq");
+            String but1=request.getParameter("atc");
+            String but2=request.getParameter("bill");
+            
+            if(but1 != null){
+                Cookie ck=new Cookie(proName,proQuan);
+                //ck.setMaxAge(-1);
+                response.addCookie(ck);
+                response.sendRedirect("shop.html");
+            }
+            if(but2 != null){
+                out.println("<h1>You have Purchased</h1>");
+                Cookie ck[]=request.getCookies();
+                for(Cookie c:ck){
+                    out.println(c.getName()+" : "+c.getValue()+"<br/>");
+                }
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
